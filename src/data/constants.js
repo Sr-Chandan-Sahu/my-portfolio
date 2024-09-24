e7e2ad921d658537208183f4861061905a9c896d
Chandan Sahu <cs154896@gmail.com>
2024-09-24 12:06:35 +0530

    Resume-updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Sr-Chandan-Sahu",
     resume:
-      "https://drive.google.com/file/d/11uMsTULSGqqTjS4A3WKdWKSTWwK4B-Lx/view?usp=sharing",
+      "https://drive.google.com/file/d/1UA7ehHV8f4MQxi_UeVORZBnDKvmFkvcn/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/sr-chandan-sahu/",
     // twitter: "https://twitter.com/",
     // insta: "https://www.instagram.com/",
